283e7cb70a68d844072b22f56701abbdc371754d
Your Name <you@example.com>
2024-04-12 13:29:09 +0300

    qweqwe

---
+++ ftp_server/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#service snmpd restart
+#service ssh start
+#tigervncserver -xstartup /usr/bin/startxfce4 -geometry 1600x900 -localhost no :1
+#websockify --web=/usr/share/novnc/ --cert=/home/ubuntu/novnc.pem 8080 localhost:5901
+#useradd john -s /bin/bash -b /home -p $(perl -e "print crypt('@-.+KHQSKO?T23^K%*OL?@9#GIC','sa');")
+#mkdir /home/john
+tail -f /dev/null
+
